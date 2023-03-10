4ec8f53d1379c733665a8c9b684c5e842c436b43
4Sitam4 <109106334+4Sitam4@users.noreply.github.com>
2023-03-10 16:01:04 +0100

    Add fonction to script.js and Add fonction to product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,13 @@
 // Récupérer l'ID du produit
-const url = new URL(window.location.href);
-const id = url.searchParams.get("id");
-console.log(id);
+function getId() {
+  const url = new URL(window.location.href);
+  const id = url.searchParams.get("id");
+  console.log(id);
+  return id;
+}
 
 // Définir les emplacements d'insertion des données
+// TODO: A transformer en fonction
 const imgKanap = document.querySelector(".item__img");
 const nomKanap = document.querySelector("#title");
 const prixKanap = document.querySelector("#price");
@@ -12,7 +16,18 @@ const optionKanap = document.querySelector("#colors");
 const qtyKanap = document.querySelector("#quantity");
 
 // Récupérer les données de l'API associées à l'ID
-fetch("http://localhost:3000/api/products/" + id)
-  .then(response => response.json())
-  .then(selectProduct => {
-    console.log(selectProduct)});
+function fetchProductInfo(id) {
+  fetch("http://localhost:3000/api/products/" + id)
+    .then(response => response.json())
+    .then(selectProduct => {
+      console.log(selectProduct);
+    });
+}
+// Appel des fonctions
+var id = getId();
+fetchProductInfo(id);
+
+// fetch("http://localhost:3000/api/products/" + id)
+//   .then(response => response.json())
+//   .then(selectProduct => {
+//     console.log(selectProduct)});
--- front/js/script.js
+++ front/js/script.js
@@ -1,53 +1,48 @@
 // Récupérer la balise HTML du fichier index.html
 // Balise HTML section avec l'id items
-const section = document.querySelector('#items');
-
-// Requet sur l'api pour récupérer les données
-fetch("http://localhost:3000/api/products")
-.then(reponse => reponse.json())
-.then(data => displayCanaps(data));
-
-// Créer une boucle pour afficher les données
-function displayCanaps (canaps){
-    for (let index = 0; index < canaps.length; index++) {
-        const {_id, name, description, imageUrl, altText} = canaps[index]
-
-
-        //Création de lien
-        const link = document.createElement("a")
-        link.href = "./product.html?id=" + _id 
-
-        //Création de l'article
-        const article = document.createElement("article");
-
-        //Création de la balise img
-        const img = document.createElement("img")
-        img.src = imageUrl
-        img.alt = altText
-        article.appendChild(img)
-
-        //Création du nom de l'article
-        const h3 = document.createElement("h3")
-        h3.className = "productName"
-        h3.innerText = name
-        article.appendChild(h3)
-
-        //Création du nom de l'article
-        const p = document.createElement("p")
-        p.className = "productDescription"
-        p.innerText = description
-        article.appendChild(p)
-
-        link.appendChild(article)
-        section.appendChild(link)
-        
-    }
+function fetchProducts() {
+  // Requet sur l'api pour récupérer les données
+  fetch("http://localhost:3000/api/products")
+    .then(reponse => reponse.json())
+    .then(data => displayCanaps(data));
 }
 
-/* <a href="./product.html?id=42">
-            <article>
-              <img src="../../back/images/kanap01.jpeg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-              <h3 class="productName">Kanap name1</h3>
-              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-            </article>
-          </a> */
+// Créer une boucle pour afficher les données
+function displayCanaps(canaps) {
+  const section = document.querySelector('#items');
+  for (let index = 0; index < canaps.length; index++) {
+    const { _id, name, description, imageUrl, altText } = canaps[index];
+
+
+    //Création de lien
+    const link = document.createElement("a");
+    link.href = "./product.html?id=" + _id;
+
+    //Création de l'article
+    const article = document.createElement("article");
+
+    //Création de la balise img
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.alt = altText;
+    article.appendChild(img);
+
+    //Création du nom de l'article
+    const h3 = document.createElement("h3");
+    h3.className = "productName";
+    h3.innerText = name;
+    article.appendChild(h3);
+
+    //Création du nom de l'article
+    const p = document.createElement("p");
+    p.className = "productDescription";
+    p.innerText = description;
+    article.appendChild(p);
+
+    link.appendChild(article);
+    section.appendChild(link);
+
+  }
+}
+
+fetchProducts();
