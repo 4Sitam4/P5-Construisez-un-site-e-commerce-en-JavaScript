9835013e7b625f86eb9ceeab10a07aef3e90444e
4Sitam4 <109106334+4Sitam4@users.noreply.github.com>
2023-04-14 15:52:41 +0200

    Ajout commentaires
    Ajout commentaires + suppression de code inutile (qty)

--- front/js/product.js
+++ front/js/product.js
@@ -8,32 +8,31 @@ function getId() {
 
 // Définir les emplacements d'insertion des données
 function displayProduct(selectProduct) {
+  // création de la balise img dans le html et insertion de l'image
   const imgKanap = document.querySelector(".item__img");
   const img = document.createElement("img");
   img.src = selectProduct.imageUrl;
   img.alt = selectProduct.altTxt;
   imgKanap.appendChild(img);
 
+  // insertion du nom du produit dans la balise h1
   const nomKanap = document.querySelector("#title");
   nomKanap.innerText = selectProduct.name;
 
+  // insertion du prix du produit dans la balise p
   const prixKanap = document.querySelector("#price");
   prixKanap.innerText = selectProduct.price;
 
+  // insertion de la description du produit dans la balise p
   const descriptionKanap = document.querySelector("#description");
   descriptionKanap.innerText = selectProduct.description;
 
+  // insertion des options de couleurs dans la balise select
   const optionKanap = document.querySelector("#colors");
   for (let index = 0; index < selectProduct.colors.length; index++) {
     const option = document.createElement("option");
     option.innerText = selectProduct.colors[index];
     optionKanap.appendChild(option);
-  }
-  const qtyKanap = document.querySelector("#quantity");
-  for (let index = 1; index < 101; index++) {
-    const option = document.createElement("option");
-    option.innerText = index;
-    qtyKanap.appendChild(option);
   }
 }
 
